26ce7e540c0f212c9c58027f94b3cb834c42dd0f
Kingsley Brown <kingsleybrownjr@yahoo.com>
2021-05-06 19:23:16 -0500

    created a methon on the quizbrain class, called next question

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from question_model import Question
 from data import question_data
+
+question_bank = []
+
+for content in question_data:
+    question_text = content['text']
+    question_answer = content['answer']
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
