8582bb5f42d59e24f14cb30675681510e95d2e42
Kingsley Brown <kingsleybrownjr@yahoo.com>
2021-05-06 20:04:56 -0500

    Quiz Game
    A simple quiz game using OOP, trivia questions from Trivia Api

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for content in question_data:
-    question_text = content['text']
-    question_answer = content['answer']
+    question_text = content['question']
+    question_answer = content['correct_answer']
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,11 +1,34 @@
 class QuizBrain:
 
     def __init__(self, q_list):
+        self.score = 0
         self.question_number = 0
         self.question_list = q_list
 
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
     def next_question(self):
-        current_question = self.question_list[self.question_number]
-        self.question_number += 1
-        ask_question = input(
-            f"{self.question_number }: {current_question.text}. (True or False)?: ")
+        while self.still_has_questions():
+            current_question = self.question_list[self.question_number]
+            self.question_number += 1
+            user_answer = input(
+                f"{self.question_number }: {current_question.text}. (True or False)?: ")
+            self.check_answer(user_answer, current_question.answer)
+
+        if not self.still_has_questions():
+            self.game_over()
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print('You got it right!')
+        else:
+            print('That\'s wrong. ')
+        print(f'The correct answer was: {correct_answer}.')
+        print(f'Your current score is {self.score}/{self.question_number}')
+        print('\n')
+
+    def game_over(self):
+        print('You\'ve completed the quiz!')
+        print(f'Your final score was: {self.score}/{self.question_number}')
